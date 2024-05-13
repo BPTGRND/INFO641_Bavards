db8dadd188e0d10a706b92b7969c8c6d6bc497b3
BPTGRND <baptiste.renand@gmx.com>
2024-05-13 13:44:04 +0200

    Création de differents dossiers & fichiers

---
+++ classes/PapotageEvent.java
@@ -0,0 +1,11 @@
+public class PapotageEvent {
+    // ATTRIBUTS
+    private String sujet;
+    private String corps;
+
+    // CONSTRUCTEUR
+    public PapotageEvent(String sujet, String corps) {
+        this.sujet = sujet;
+        this.corps = corps;
+    }
+}
